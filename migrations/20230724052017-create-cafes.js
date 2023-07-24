f574f7d7674b890bca147b1303dd049dc7221b94
goh rui chuan <gohruichuan@gmail.com>
2023-07-24 13:26:53 +0800

    fix: cafes migration

--- migrations/20230724052017-create-cafes.js
+++ migrations/20230724052017-create-cafes.js
@@ -1,13 +1,13 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up(queryInterface, DataTypes) {
     await queryInterface.createTable("Cafes", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.UUID,
+        type: DataTypes.UUID,
       },
       name: {
         type: DataTypes.STRING(255),
@@ -27,15 +27,15 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
+        type: DataTypes.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
+        type: DataTypes.DATE,
       },
     });
   },
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface, DataTypes) {
     await queryInterface.dropTable("Cafes");
   },
 };
