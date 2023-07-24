1e8bd364551204e522b80b04d1c5489cc9c701d6
goh rui chuan <gohruichuan@gmail.com>
2023-07-24 13:20:26 +0800

    add: cafes migrations

---
+++ models/cafes.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cafes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Cafes.init({
+    id: DataTypes.UUID,
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    logo: DataTypes.STRING,
+    location: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Cafes',
+  });
+  return Cafes;
+};
