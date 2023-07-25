06097b85eabaea1998eb370440c45e20b66a4de9
goh rui chuan <gohruichuan@gmail.com>
2023-07-25 17:06:04 +0800

    upd: employee endpoint

--- services/employees.js
+++ services/employees.js
@@ -3,6 +3,10 @@ const db = require("../models/index");
 
 const router = new express.Router();
 
+const convetToDays = (ms) => {
+  return Math.ceil(ms / (24 * 60 * 60 * 1000));
+};
+
 router.post("/", (req, res) => {});
 
 router.get("/", async (req, res) => {
@@ -10,14 +14,26 @@ router.get("/", async (req, res) => {
   let employees;
   if (cafeName) {
     // Find all cafes with cafeName
-    let cafes = await db.Cafes.findAll({
+    let cafe = await db.Cafes.findOne({
       raw: true,
       where: { name: cafeName },
     });
-    // cafes = JSON.parse(JSON.stringify(cafes));
-    console.log("cafes ", cafes);
 
     // Find all employess who worked in the cafe
+    employees = await db.Employees.findAll({
+      raw: true,
+      where: { cafeId: cafe.id },
+    });
+
+    // Format days_worked and cafe for employees
+    employees.map((employee) => {
+      const currentTime = new Date().getTime();
+      const employeeTime = new Date(employee.start_date).getTime();
+
+      const timeDiffInDays = convetToDays(currentTime - employeeTime);
+      employee.days_worked = timeDiffInDays;
+      employee.cafe = cafe.name;
+    });
   }
 
   res.json(employees);
