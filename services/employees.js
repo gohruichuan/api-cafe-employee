fc6cbade363df150113e467c7ee06f70ff053a57
goh rui chuan <gohruichuan@gmail.com>
2023-07-25 17:42:36 +0800

    upd: cafe endpoint

--- services/employees.js
+++ services/employees.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const express = require("express");
 const db = require("../models/index");
 
@@ -10,33 +11,48 @@ const convetToDays = (ms) => {
 router.post("/", (req, res) => {});
 
 router.get("/", async (req, res) => {
-  const cafeName = req.query.cafe;
+  const query = req.query.cafe;
   let employees;
-  if (cafeName) {
-    // Find all cafes with cafeName
-    let cafe = await db.Cafes.findOne({
-      raw: true,
-      where: { name: cafeName },
-    });
+  const schema = Joi.string().optional();
 
-    // Find all employess who worked in the cafe
-    employees = await db.Employees.findAll({
-      raw: true,
-      where: { cafeId: cafe.id },
-    });
+  try {
+    const cafeName = await schema.validateAsync(query);
+    if (cafeName) {
+      // Find all cafes with cafeName
+      let cafe = await db.Cafes.findOne({
+        raw: true,
+        where: { name: cafeName },
+      });
 
-    // Format days_worked and cafe for employees
-    employees.map((employee) => {
-      const currentTime = new Date().getTime();
-      const employeeTime = new Date(employee.start_date).getTime();
+      if (cafe) {
+        // Find all employess who worked in the cafe
+        employees = await db.Employees.findAll({
+          raw: true,
+          where: { cafeId: cafe.id },
+        });
+        console.log("employees ", employees);
 
-      const timeDiffInDays = convetToDays(currentTime - employeeTime);
-      employee.days_worked = timeDiffInDays;
-      employee.cafe = cafe.name;
-    });
+        // Format days_worked and cafe for employees
+        employees.map((employee) => {
+          const currentTime = new Date().getTime();
+          const employeeTime = new Date(employee.start_date).getTime();
+
+          const timeDiffInDays = convetToDays(currentTime - employeeTime);
+          employee.days_worked = timeDiffInDays;
+          employee.cafe = cafe.name;
+        });
+        res.json(employees);
+      } else {
+        res.status(400);
+        res.send("No cafe found: " + cafeName);
+        return res;
+      }
+    }
+  } catch (err) {
+    res.status(400);
+    res.send(err);
+    return res;
   }
-
-  res.json(employees);
 });
 
 module.exports = router;
