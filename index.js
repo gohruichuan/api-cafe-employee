abe145e8ef7db5f16f9853433a2a4e3a19019e9d
goh rui chuan <gohruichuan@gmail.com>
2023-07-27 11:51:32 +0800

    add: cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const util = require("./utils.js");
 
 const cafesController = require("./services/cafes");
@@ -9,6 +9,7 @@ const employeesController = require("./services/employees");
 
 util.connectDB();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
